2537c529fee52eb46ffc24764f7c65e9e5384c72
Limush <dzaikin999@gmail.com>
2025-04-25 12:07:56 +0300

    Full

--- ТПСГ ЛР6.py
+++ ТПСГ ЛР6.py
@@ -20,13 +20,21 @@ def Print_table(data1, data2, data3, data4):
     return print(table)
 
 
+def Count(mass):
+    x, y = [], []
+    for i in range(len(mass)):
+        x.append(mass[i])
+        y.append(i)
+    return x, y
+
+
 figure, axis = plt.subplots(2, 2, figsize=(15, 8))
 
-data1 = LR1(1, 106, 0, 500, 27)
+data1 = LR1(7, 106, 1283, 6075, 500)
 
-data2 = LR2(2, 5, 1, 3, 6, 30)
+data2 = LR2(40, 5, 57, 45, 512, 500)
 
-data3 = LR3('x**7+x**4+1', 1, '1110001', 1)
+data3 = LR3('x**11+x**2+1', 2, '11111111100', 1)
 
 data4 = LR4('x**4+x**3+1', 1, 1, '1100',
             'x**4+1', 1, 1, '1101',
@@ -38,19 +46,23 @@ print(f"Последовательность 1:\n\t{data1}\n"
       f"Последовательность 3:\n\t{data3}\n"
       f"Последовательность 4:\n\t{data4}\n")
 
-axis[0, 0].scatter(data1, [0 for _ in data1], color="red", s=10)
+x1, y1 = Count(data1)
+axis[0, 0].scatter(y1, x1, color="black", s=5)
 axis[0, 0].set_xlabel(r'$x$', fontsize=14)
 axis[0, 0].set_ylabel(r'$y$', fontsize=14, rotation=0)
 
-axis[0, 1].scatter(data2, [0 for _ in data2], color="red", s=10)
+x2, y2 = Count(data2)
+axis[0, 1].scatter(x2, y2, color="black", s=5)
 axis[0, 1].set_xlabel(r'$x$', fontsize=14)
 axis[0, 1].set_ylabel(r'$y$', fontsize=14, rotation=0)
 
-axis[1, 0].scatter(data3, [0 for _ in data3], color="red", s=10)
+x3, y3 = Count(data3)
+axis[1, 0].scatter(x3, y3, color="black", s=5)
 axis[1, 0].set_xlabel(r'$x$', fontsize=14)
 axis[1, 0].set_ylabel(r'$y$', fontsize=14, rotation=0)
 
-axis[1, 1].scatter(data4, [0 for _ in data4], color="red", s=10)
+x4, y4 = Count(data4)
+axis[1, 1].scatter(x4, y4, color="black", s=5)
 axis[1, 1].set_xlabel(r'$x$', fontsize=14)
 axis[1, 1].set_ylabel(r'$y$', fontsize=14, rotation=0)
 
